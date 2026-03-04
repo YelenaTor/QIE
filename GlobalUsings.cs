f077a4f11c6329a92602a332530f7db951586677
YoruSprites <dngentle@gmail.com>
2026-03-05 01:50:52 +0200

    fix: Add GlobalUsings.cs, remove stale ImGui ref, fix workflow quality param

---
+++ GlobalUsings.cs
@@ -0,0 +1,11 @@
+// Global using directives for Quartermaster.
+// The Dalamud.NET.Sdk disables implicit usings, so we declare them here once.
+
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Text.Json.Serialization;
+global using System.Threading;
+global using System.Threading.Tasks;
